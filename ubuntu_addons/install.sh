53325512d3b8ab4e289a266c06c2373cb9c6c5c8
Nathan Young <75765999+Pizza2d1@users.noreply.github.com>
2025-03-15 12:26:32 -0600

    wrong dir location

--- ubuntu_addons/install.sh
+++ ubuntu_addons/install.sh
@@ -10,8 +10,8 @@ else
     read -p "Do you want to download the \"Desktop Cube\" extension? [Y/n]" uinput
 fi
 if [[ -z $uinput ]] || [[ $uinput == "y" ]] || [[ $uinput == "Y" ]]; then
-    mkdir home/$user/environment_maker_downloads
-    cd home/$user/environment_maker_downloads
+    mkdir /home/$user/environment_maker_downloads
+    cd /home/$user/environment_maker_downloads
     git clone https://github.com/Schneegans/Desktop-Cube.git
     cd Desktop-Cube
     make install
@@ -26,8 +26,8 @@ else
     read -p "Do you want to download the \"Unblank Lock Ccreen\" extension? [Y/n]" uinput
 fi
 if [[ -z $uinput ]] || [[ $uinput == "y" ]] || [[ $uinput == "Y" ]]; then
-    mkdir home/$user/environment_maker_downloads
-    cd home/$user/environment_maker_downloads
+    mkdir /home/$user/environment_maker_downloads
+    cd /home/$user/environment_maker_downloads
     git clone https://github.com/sunwxg/gnome-shell-extension-unblank.git
     cd gnome-shell-extension-unblank
     make install
@@ -41,10 +41,10 @@ else
     read -p "Do you want to download the \"Spotify Widget\" extension? [Y/n]" uinput
 fi
 if [[ -z $uinput ]] || [[ $uinput == "y" ]] || [[ $uinput == "Y" ]]; then
-    mkdir home/$user/environment_maker_downloads
-    cd home/$user/environment_maker_downloads
+    mkdir /home/$user/environment_maker_downloads
+    cd /home/$user/environment_maker_downloads
     git clone https://extensions.gnome.org/extension/7406/spotify-controls/
-    cp spotify-controls/ home/$user/.local/share/gnome-shell/extensions/spotify-controls@Sonath21/
+    cp spotify-controls/ /home/$user/.local/share/gnome-shell/extensions/spotify-controls@Sonath21/
     echo "\033[0m\e[92mComplete!\033[0m"
 fi
 
@@ -55,8 +55,8 @@ else
     read -p "Do you want to download the \"System Monitor Widget\" extension? [Y/n]" uinput
 fi
 if [[ -z $uinput ]] || [[ $uinput == "y" ]] || [[ $uinput == "Y" ]]; then
-    mkdir home/$user/environment_maker_downloads
-    cd home/$user/environment_maker_downloads
+    mkdir /home/$user/environment_maker_downloads
+    cd /home/$user/environment_maker_downloads
     echo "Installing additional dependancies"
     sudo apt install gir1.2-gtop-2.0 gir1.2-nm-1.0 gir1.2-clutter-1.0 gnome-system-monitor -y
     if [ $? -ne 0 ]; then
@@ -77,8 +77,8 @@ else
     read -p "Do you want to download the \"Battery Charge Limitere\" extension? [Y/n]" uinput
 fi
 if [[ -z $uinput ]] || [[ $uinput == "y" ]] || [[ $uinput == "Y" ]]; then
-    mkdir home/$user/environment_maker_downloads
-    cd home/$user/environment_maker_downloads
+    mkdir /home/$user/environment_maker_downloads
+    cd /home/$user/environment_maker_downloads
     echo "Installing additional dependancies"
     sudo apt install gettext -y 
     if [ $? -ne 0 ]; then
@@ -97,10 +97,10 @@ fi
 # # SPOTIFY
 # read -p "Do you want to download the \"Spotify Widget\" extension? [Y/n]" uinput
 # if [[ -z $uinput ]] || [[ $uinput == "y" ]] || [[ $uinput == "Y" ]]; then
-#     if [[ ! -d home/$user/environment_maker_downloads ]]; then
-#         mkdir home/$user/environment_maker_downloads
+#     if [[ ! -d /home/$user/environment_maker_downloads ]]; then
+#         mkdir /home/$user/environment_maker_downloads
 #     fi
-#     cd home/$user/environment_maker_downloads
+#     cd /home/$user/environment_maker_downloads
 #     git clone https://extensions.gnome.org/extension/7406/spotify-controls/
 #     cp spotify-controls/ spotify-controls@Sonath21/
 #     echo "\033[0m\e[92mComplete!\033[0m"
@@ -115,7 +115,7 @@ fi
 # read -p "Do you want to download the \"Desktop Cube\" extension? [Y/n]" uinput
 
 # if [[ -z $uinput ]] || [[ $uinput == "y" ]] || [[ $uinput == "Y" ]]; then
-#     mkdir home/$user/environment_maker_downloads
-#     cd home/$user/environment_maker_downloads
+#     mkdir /home/$user/environment_maker_downloads
+#     cd /home/$user/environment_maker_downloads
 
 # fi
